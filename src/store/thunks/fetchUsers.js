e3729c62ec1023fd21f8b57c647047d11c1d3a91
Denis Ouma <dennisouma38@gmail.com>
2023-04-21 10:10:16 +0300

    Loading skeleton component created

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -2,7 +2,15 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
 	const res = await axios.get("http://localhost:3001/users");
+	await pause(4000);
 	return res.data;
 });
+
+// dev Only
+const pause = (duration) => {
+	return new Promise((resolve) => {
+		setTimeout(resolve, duration);
+	});
+};
 
 export { fetchUsers };
